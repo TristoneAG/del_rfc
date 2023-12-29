284a0a24f54ecfbedeb2393515e112645f7bdb33
TristoneAG <aram.guillen@tristone.com>
2023-12-29 12:46:11 -0600

    fix(DEL_RFC): :bug: Removing unecessary route
    Removing RabbitMQ route not use for the moment

--- backend_server.js
+++ backend_server.js
@@ -8,7 +8,7 @@ const routes_VUL = require('./routes/routes_VUL')
 const routes_EXT = require('./routes/routes_EXT')
 const routes_SEM = require('./routes/routes_SEM')
 const routes_RW = require('./routes/routes_RW')
-const routes_RABBITMQ = require('./routes/routes_RABBITMQ')
+
 const portNumber = process.env.port || process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,7 +19,7 @@ app.use(routes_VUL)
 app.use(routes_EXT)
 app.use(routes_SEM)
 app.use(routes_RW)
-app.use(routes_RABBITMQ)
+
 
 let server = app.listen(portNumber, function () {
   console.info('Express node_env: ' + process.env.NODE_ENV + " Port: " + server.address().port);
