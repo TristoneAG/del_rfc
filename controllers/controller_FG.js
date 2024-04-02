61434f6d68d18faa9fcea3258fc2d65c21b2c942
TristoneAG <aram.guillen@tristone.com>
2024-04-02 15:13:04 -0600

    fix(DEL_RFC): :bug: Fixing employee not visible on labels and database
    Fixing wrong name on variable employee when printing MES labels

--- controllers/controller_FG.js
+++ controllers/controller_FG.js
@@ -4,8 +4,8 @@ const funcion = require('../functions/functions_FG');
 
 controller.createMESHU_POST = async (req, res) => {
     try {
-        const { material, quantity, employee_id, station, plant, packInstruction, PACKNR, printer } = req.body;
-        const result = await funcion.createMESHU(material, quantity, employee_id, station, plant, packInstruction, PACKNR, printer );
+        const { material, quantity, employee, station, plant, packInstruction, PACKNR, printer } = req.body;
+        const result = await funcion.createMESHU(material, quantity, employee, station, plant, packInstruction, PACKNR, printer );
         res.status(200).json(result);
     } catch (error) {
         res.status(500).json( {"error": error.message});
